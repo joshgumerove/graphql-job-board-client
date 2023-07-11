b8518c370ab0062c7bc7ee1b53cad4a60e03f5e3
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-07-11 16:21:30 -0400

    fetch jobs from client

---
+++ src/lib/graphql/queries.js
@@ -0,0 +1,23 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const client = new GraphQLClient("http://localhost:9000/graphql");
+
+async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        title
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await client.request(query);
+  return jobs;
+}
+
+export { getJobs };
